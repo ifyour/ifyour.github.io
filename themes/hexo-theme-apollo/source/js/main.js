592a51ebf879ed3a7866d70a4c8a54c6fd6c2d30
ifyour <ifyour@outlook.com>
2018-12-24 12:39:12 +0800

    feat: debounce search event

--- themes/hexo-theme-apollo/source/js/main.js
+++ themes/hexo-theme-apollo/source/js/main.js
@@ -21,6 +21,20 @@
     var EXP_ISMOBILE = /(micromessenger|phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i;
     return EXP_ISMOBILE.test(ua);
   };
+  var debounce = function (fn ,wait) {
+    var timer = null;
+    return function () {
+      var context = this;
+      var args = arguments;
+      if (timer) {
+          clearTimeout(timer);
+          timer = null;
+      }
+      timer = setTimeout(function () {
+          fn.apply(context, args)
+      }, wait);
+    }
+  }
 
   var Blog = {
     toc: (function() {
@@ -87,26 +101,26 @@
       var render = function (data) {
         searchResult.innerHTML = template('search-tmp', {list: data});
       }
+      var searching = function () {
+        var value = searchInput.value.trim();
+        if (value) {
+          query(value).then(function (result) {
+            var tmpData = result.map(function (item) {
+              return {
+                title: item._highlightResult.title.value,
+                permalink: item.permalink.replace('http://' + w.location.host, ''),
+                summary: item._highlightResult.excerptStrip.value,
+              }
+            })
+            render(tmpData);
+          })
+        } else {
+          searchResult.innerHTML = '';
+        }
+      }
       return {
         active: function() {
-
-          searchInput.addEventListener('input', function () {
-            var value = searchInput.value.trim();
-            if (value) {
-              query(value).then(function (result) {
-                var tmpData = result.map(function (item) {
-                  return {
-                    title: item._highlightResult.title.value,
-                    permalink: item.permalink.replace('http://' + w.location.host, ''),
-                    summary: item._highlightResult.excerptStrip.value,
-                  }
-                })
-                render(tmpData);
-              })
-            } else {
-              searchResult.innerHTML = '';
-            }
-          })
+          searchInput.addEventListener('input', debounce(searching, 500));
 
           navSearchText.addEventListener('click', function() {
             searchModal.classList.add('active');
